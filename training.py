65c017ac4bf074edd91a965d1d072915d2d8bbd5
Juan De Dios Santos <juan.d.diossantos@gmail.com>
2019-04-09 22:16:29 +0200

    Changes to training dataset

--- training.py
+++ training.py
@@ -5,15 +5,15 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-df = pd.read_csv('potato_df_weekday_hour.csv', encoding='utf-8')
+df = pd.read_csv('potato_df_weekday_hour_new.csv', encoding='utf-8')
 X_train = df[['weekday', 'hour']]
 
 clf = OneClassSVM(nu=0.1, kernel="rbf", gamma=0.01)
+# try this one for a very overfitted example
 # clf = OneClassSVM(kernel="rbf")
 clf.fit(X_train)
 
-
-
+# plot of the decision frontier
 xx, yy = np.meshgrid(np.linspace(-5, 15, 500), np.linspace(-5, 30, 500))
 Z = clf.decision_function(np.c_[xx.ravel(), yy.ravel()])
 Z = Z.reshape(xx.shape)
