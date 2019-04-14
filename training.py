04d2ecbcef32d314ba457576b00144a2a4d9269a
Juan De Dios Santos <juan.d.diossantos@gmail.com>
2019-04-14 17:48:49 +0200

    Renamed df and added comments to training

--- training.py
+++ training.py
@@ -1,3 +1,9 @@
+"""
+This script fits a One Class SVM using Potato Salad data
+Code for plotting the decision function was taken from:
+https://scikit-learn.org/stable/auto_examples/svm/plot_oneclass.html#sphx-glr-auto-examples-svm-plot-oneclass-py
+"""
+
 from sklearn.svm import OneClassSVM
 from sklearn.model_selection import GridSearchCV
 import pandas as pd
@@ -5,7 +11,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-df = pd.read_csv('potato_df_weekday_hour_new.csv', encoding='utf-8')
+df = pd.read_csv('potato_training_df.csv', encoding='utf-8')
 X_train = df[['weekday', 'hour']]
 
 clf = OneClassSVM(nu=0.1, kernel="rbf", gamma=0.01)
@@ -17,8 +23,9 @@ clf.fit(X_train)
 xx, yy = np.meshgrid(np.linspace(-5, 15, 500), np.linspace(-5, 30, 500))
 Z = clf.decision_function(np.c_[xx.ravel(), yy.ravel()])
 Z = Z.reshape(xx.shape)
-plt.title("Novelty Detection")
-plt.contourf(xx, yy, Z, levels=np.linspace(Z.min(), 0, 7), cmap=plt.cm.PuBu)
+plt.title("Potato Salad Decision Boundary")
+# comment out the next line to see the "ripples" of the boundary
+# plt.contourf(xx, yy, Z, levels=np.linspace(Z.min(), 0, 7), cmap=plt.cm.PuBu)
 a = plt.contour(xx, yy, Z, levels=[0], linewidths=2, colors='darkred')
 plt.contourf(xx, yy, Z, levels=[0, Z.max()], colors='palevioletred')
 b1 = plt.scatter(X_train.iloc[:,0], X_train.iloc[:,1])
